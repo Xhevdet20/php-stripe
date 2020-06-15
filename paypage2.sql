39eb6839eca0a50ae357464e3b3d4f924361b819
xhevdet <xhevdet.kryeziu@kujtesa.com>
2020-06-15 10:35:19 +0200

    added SQL for the database

--- paypage2.sql
+++ paypage2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 13, 2020 at 09:41 AM
+-- Generation Time: Jun 15, 2020 at 08:34 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.2.0
 
@@ -47,7 +47,8 @@ INSERT INTO `costumers` (`id`, `first_name`, `last_name`, `email`, `created_at`)
 ('cus_HSQaDD52RRIBmL', 'john ', 'due', 'jon@doe.com', '2020-06-13 11:32:51'),
 ('cus_HSQbFmGWdkx5By', 'john ', 'due', 'jon@doe.com', '2020-06-13 11:33:27'),
 ('cus_HSQcWZMWvGZcgh', 'john', 'die', 'jon@die.com', '2020-06-13 11:34:31'),
-('cus_HSQeoYMvI9bRLr', 'jon', 'due', 'jon@due.com', '2020-06-13 11:36:46');
+('cus_HSQeoYMvI9bRLr', 'jon', 'due', 'jon@due.com', '2020-06-13 11:36:46'),
+('cus_HT9zbWoapB4Wb3', 'hashim', 'thaqi', 'hasha12345@youtube.com', '2020-06-15 10:28:00');
 
 -- --------------------------------------------------------
 
@@ -72,7 +73,8 @@ CREATE TABLE IF NOT EXISTS `transactions` (
 --
 
 INSERT INTO `transactions` (`id`, `costumer_id`, `product`, `amount`, `currency`, `status`, `created_at`) VALUES
-('cus_HSQeoYMvI9bRLr', 'cus_HSQeoYMvI9bRLr', 'Intro To Angular Course', '5000', 'usd', 'succeeded', '2020-06-13 11:36:46');
+('cus_HSQeoYMvI9bRLr', 'cus_HSQeoYMvI9bRLr', 'Intro To Angular Course', '5000', 'usd', 'succeeded', '2020-06-13 11:36:46'),
+('cus_HT9zbWoapB4Wb3', 'cus_HT9zbWoapB4Wb3', 'Intro To Angular Course', '5000', 'usd', 'succeeded', '2020-06-15 10:28:00');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
